4e9eb86bec5f6a6965029e67d4bd23d2889ca739
Ryan Burnette <ryan.burnette@gmail.com>
2017-02-28 12:59:56 -0500

    flesh out flight spec

--- spec/lib/skedplus/data/flight_spec.rb
+++ spec/lib/skedplus/data/flight_spec.rb
@@ -1,4 +1,45 @@
 require "skedplus/data/flight"
 
 RSpec.describe Skedplus::Data::Flight do
+  before :all do
+  end
+
+  it "#sequence" do
+  end
+
+  it "#number" do
+  end
+
+  it "#tail" do
+  end
+
+  it "#org" do
+  end
+
+  it "#dest" do
+  end
+
+  it "#dep" do
+  end
+
+  it "#arr" do
+  end
+
+  it "#pax" do
+  end
+
+  it "#block" do
+  end
+
+  it "#credit" do
+  end
+
+  it "#dpu" do
+  end
+
+  it "#dhd" do
+  end
+
+  it "#turn" do
+  end
 end
