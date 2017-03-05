157b763721e1b1b25109c99e4da268cb6517820e
kruzapalooza <mkruza@gmail.com>
2017-03-05 12:50:26 -0500

    Added crewmember model and spec tests

--- spec/lib/skedplus_pairing/crewmember_spec.rb
+++ spec/lib/skedplus_pairing/crewmember_spec.rb
@@ -1,17 +1,26 @@
 RSpec.describe SkedplusPairing::Crewmember do
   FakeParser = Struct.new(:name, :base, :equipment)
-  let(:fake_parser) { FakeParser.new(Faker::Name.name, Faker::Address.city, Faker::Commerce.product_name) }
+  let(:fake_parser) { FakeParser.new("Marc Kruza", "CHS", "CRJ") }
+  let(:crewmember) { SkedplusPairing::Crewmember.new(fake_parser) }
 
-  it "#name"
-    expect(crewmember.name).to be_a String
+  it "#name" do
+    expect(crewmember.name).to eq "Marc Kruza"
   end
 
-  it "#base"
-    expect(crewmember.base).to be_a String
+  it "#first_name" do
+    expect(crewmember.first_name).to eq("Marc")
   end
 
-  it "#equipment"
-    expect(crewmember.base).to be_a String
+  it "#last_name" do
+    expect(crewmember.last_name).to eq("Kruza")
   end
 
+  it "#base" do
+    expect(crewmember.base).to be_a SkedplusPairing::Airport
+    expect(crewmember.base.timezone).to eq "America/New_York"
+  end
+
+  it "#equipment" do
+    expect(crewmember.equipment).to eq "CRJ"
+  end
 end
