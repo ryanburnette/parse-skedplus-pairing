903b3e5920870a5dab3e05e4ec12a8b9e5f58cb7
kruzapalooza <mkruza@gmail.com>
2017-03-04 19:09:42 -0500

    started crewmember_spec.rb

--- spec/lib/skedplus_pairing/crewmember_spec.rb
+++ spec/lib/skedplus_pairing/crewmember_spec.rb
@@ -1,2 +1,17 @@
 RSpec.describe SkedplusPairing::Crewmember do
+  FakeParser = Struct.new(:name, :base, :equipment)
+  let(:fake_parser) { FakeParser.new(Faker::Name.name, Faker::Address.city, Faker::Commerce.product_name) }
+
+  it "#name"
+    expect(crewmember.name).to be_a String
+  end
+
+  it "#base"
+    expect(crewmember.base).to be_a String
+  end
+
+  it "#equipment"
+    expect(crewmember.base).to be_a String
+  end
+
 end
