889a944c17bf6617e086513ab68f049fd2407ba3
Ryan Burnette <ryan.burnette@gmail.com>
2017-03-06 21:10:46 -0500

    take that out

--- spec/lib/skedplus_pairing/airport_spec.rb
+++ spec/lib/skedplus_pairing/airport_spec.rb
@@ -1,10 +1,6 @@
 RSpec.describe SkedplusPairing::Airport do
   let(:atl) { SkedplusPairing::Airport.new("ATL") }
 
-  it "instantiates" do
-    expect(atl).to be_a SkedplusPairing::Airport
-  end
-
   it "#name" do
     expect(atl.name).to eq "Hartsfield Jackson Atlanta Intl"
   end
